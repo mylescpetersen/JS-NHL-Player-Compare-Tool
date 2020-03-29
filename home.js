731c31b2fb4bd295e0ae897da122535a00cf74bd
mylescpetersen <mylescpetersen@gmail.com>
2020-03-28 23:16:49 -0700

    dual comparison works, just needs little tweaks

--- home.js
+++ home.js
@@ -1,60 +1,86 @@
-function searchPlayer() {
-    let flg = false;
-    input = $("#playerName").val();
-    if (input === "") {
+/*
+TODOS:
+Either add goalie comparison or alert if player position is G (maybe use 'if any values are undefined, throw error'
+Change css color of greater stat to green (can prob be done only in css)
+Fixes throughout home.js
+
+
+ */
+
+
+function calculatePlayers() {
+    let playerName0 = $("#playerName0").val();
+    // change to if at least one name empty
+    //TODO FIX IF EMPTY INPUT (Does nothing)
+    if (isEmpty(playerName0) || isEmpty(playerName1)) {
         alert("Please enter a name.");
         return;
+    } else {
+        let playerId0 = searchTeams(playerName0, 0);
+        console.log(playerId0);
+        getPlayerStats(playerId0);
+
+        // add player1
+        let playerName1 = $("#playerName1").val();
+        if (!isEmpty(playerName1)) {
+            let playerId1 = searchTeams(playerName1, 1);
+            getPlayerStats(playerId1);
+        }
     }
+}
+
+function isEmpty(playerName) {
+    if (playerName0 === "") {
+        alert("Please enter a name.");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function searchTeams(playerName, numPlayer) {
+    let isFound = false;
+
     for (let i = 1; i < 31; i++) {
         if (i == 11 || i == 27) {
             //Teams don't exist so pass
         } else {
-            let curLink = 'https://statsapi.web.nhl.com/api/v1/teams/' + i + '/roster';
-            // console.log(curLink);
-            $.get(curLink, function (data) {
-                // console.log(data);
-                for (let j = 0; j < data.roster.length; j++) {
-                    let player = data.roster[j].person;
-                    if (player.fullName.toLowerCase() === input.toLowerCase()) {
-                        flg = true;
-                        getPlayerStats(player.id);
-                        return;
-
-                    }
-                }
-            });
+            findPlayer(i, numPlayer);
         }
     }
     /// does jets, coyotes, golden knights cause of weird api
-    for (let i = 52; i < 55; i++) {
+    if (isFound === false) {
+        for (let i = 52; i < 55; i++) {
+            findPlayer(i, numPlayer);
+        }
+    }
+    //TODO FIX ALERT ALWAYS APPEARS
+    // if (isFound === false) {
+    //     alert("Player not found.");
+    //     return;
+    // }
+
+    function findPlayer(i, numPlayer) {
         let curLink = 'https://statsapi.web.nhl.com/api/v1/teams/' + i + '/roster';
         // console.log(curLink);
         $.get(curLink, function (data) {
             // console.log(data);
             for (let j = 0; j < data.roster.length; j++) {
                 let player = data.roster[j].person;
-                if (player.fullName.toLowerCase() === input.toLowerCase()) {
-                    flg = true;
-                    getPlayerStats(player.id);
-                    return;
-
+                if (player.fullName.toLowerCase() === playerName.toLowerCase()) {
+                    console.log(player.id);
+                    isFound = true;
+                    getPlayerStats(player.id, numPlayer);
                 }
             }
         });
     }
-    //TODO FIX
-    flg = true;
-    if (flg === false) {
-        alert("Player not found.");
-        return;
-    }
-
 }
 
 
-function getPlayerStats(playerId) {
-
-    console.log(playerId);
+function getPlayerStats(playerId, numPlayer) {
+    // console.log(playerId);
     let results = "";
     let playerLink = 'https://statsapi.web.nhl.com/api/v1/people/' + playerId + '/stats?stats=statsSingleSeason&season=20192020'
     let printStats = {
@@ -83,58 +109,60 @@ function getPlayerStats(playerId) {
         for (let i = 0; i < checkedStats.length; i++) {
 
             let curStat = checkedStats[i]
-            // console.log(s);
-            // console.log(playerStats[s]);
+
             // TODO FIX printStats.curStat
             console.log(curStat);
             addString = curStat + ": " + playerStats[curStat];
             results += addString + '<br>';
-        }
 
-        $("#results").html(JSON.stringify(results));    //Stringify redundant?
+            let divNum = "#playerStats" + numPlayer;
+            $(divNum).html(results);
+
+        }
     });
 }
 
-let test = {
-    "copyright" : "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2020. All Rights Reserved.",
-    "stats" : [ {
-        "type" : {
-            "displayName" : "statsSingleSeason"
-        },
-        "splits" : [ {
-            "season" : "20192020",
-            "stat" : {
-                "timeOnIce" : "1415:41",
-                "assists" : 24,
-                "goals" : 6,
-                "pim" : 20,
-                "shots" : 126,
-                "games" : 56,
-                "hits" : 62,
-                "powerPlayGoals" : 2,
-                "powerPlayPoints" : 9,
-                "powerPlayTimeOnIce" : "144:07",
-                "evenTimeOnIce" : "1136:11",
-                "penaltyMinutes" : "20",
-                "faceOffPct" : 0.0,
-                "shotPct" : 4.8,
-                "gameWinningGoals" : 0,
-                "overTimeGoals" : 0,
-                "shortHandedGoals" : 0,
-                "shortHandedPoints" : 1,
-                "shortHandedTimeOnIce" : "135:23",
-                "blocked" : 123,
-                "plusMinus" : 10,
-                "points" : 30,
-                "shifts" : 1628,
-                "timeOnIcePerGame" : "25:16",
-                "evenTimeOnIcePerGame" : "20:17",
-                "shortHandedTimeOnIcePerGame" : "02:25",
-                "powerPlayTimeOnIcePerGame" : "02:34"
-            }
-        } ]
-    } ]
-}
+
+// let test = {
+//     "copyright": "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2020. All Rights Reserved.",
+//     "stats": [{
+//         "type": {
+//             "displayName": "statsSingleSeason"
+//         },
+//         "splits": [{
+//             "season": "20192020",
+//             "stat": {
+//                 "timeOnIce": "1415:41",
+//                 "assists": 24,
+//                 "goals": 6,
+//                 "pim": 20,
+//                 "shots": 126,
+//                 "games": 56,
+//                 "hits": 62,
+//                 "powerPlayGoals": 2,
+//                 "powerPlayPoints": 9,
+//                 "powerPlayTimeOnIce": "144:07",
+//                 "evenTimeOnIce": "1136:11",
+//                 "penaltyMinutes": "20",
+//                 "faceOffPct": 0.0,
+//                 "shotPct": 4.8,
+//                 "gameWinningGoals": 0,
+//                 "overTimeGoals": 0,
+//                 "shortHandedGoals": 0,
+//                 "shortHandedPoints": 1,
+//                 "shortHandedTimeOnIce": "135:23",
+//                 "blocked": 123,
+//                 "plusMinus": 10,
+//                 "points": 30,
+//                 "shifts": 1628,
+//                 "timeOnIcePerGame": "25:16",
+//                 "evenTimeOnIcePerGame": "20:17",
+//                 "shortHandedTimeOnIcePerGame": "02:25",
+//                 "powerPlayTimeOnIcePerGame": "02:34"
+//             }
+//         }]
+//     }]
+// }
 
 
 
